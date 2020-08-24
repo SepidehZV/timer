9cefb05867fcab075c07600acd70b35f21908bcf
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-24 19:40:08 +0000

    timer alarm with setTimeout

---
+++ timer1.js
@@ -0,0 +1,9 @@
+const alarmTimes = process.argv.slice(2);
+const timer = function(arr) {
+  for (let time of arr) {
+    if (Number(time) && Number(time) > 0) {
+      setTimeout(() => process.stdout.write(`${time} seconds\x07 \n`),Number(time)*1000);
+    }
+  }
+}
+timer(alarmTimes);
